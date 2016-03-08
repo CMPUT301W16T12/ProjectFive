e215472c3864b59bfc45fc74180cb36657a2a4d9
Wai Yi Low <waiyi@cs.ualberta.ca>
2016-03-07 17:00:43 -0700

    edited briefly of nearly all - xml activities and contents as well as the java codes

---
+++ src/AppFive/app/src/main/java/ca/ualberta/appfive/OwnerInfo.java
@@ -0,0 +1,26 @@
+package ca.ualberta.appfive;
+
+/**
+ * Created by waiyi on 3/7/16.
+ */
+public class OwnerInfo {
+
+    private static String name;
+    private static String email;
+    private static String mobile;
+    private static String work;
+
+    public static String getOwnerWork() {
+        return work;
+    }
+
+    public static String getOwnerMobile() {
+        return mobile;
+    }
+
+    public static String getName() {return name; }
+    public static String getEmail() {
+        return email;
+    }
+
+}
