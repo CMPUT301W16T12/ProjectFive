0820ce440d6672ca34723ddbbaf9296f2ff8a853
Andrew Whittle <awhittle@ualberta.ca>
2016-03-09 22:29:02 -0700

    ANDR: Remove references to Floating Action Buttons

--- src/AppFive/app/src/main/java/ca/ualberta/appfive/SearchResultsActivity.java
+++ src/AppFive/app/src/main/java/ca/ualberta/appfive/SearchResultsActivity.java
@@ -25,15 +25,7 @@ public class SearchResultsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_results);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
     }
 
 }
