aaec0a0599aea7e037f8dabb0de1d10ffbcc8c93
Andrew Whittle <awhittle@ualberta.ca>
2016-03-11 08:15:44 -0700

    ANDR: Fix up Book and OwnerInfo models
    Made models extend BModel
    Made constructors

--- src/AppFive/app/src/main/java/ca/ualberta/appfive/OwnerInfo.java
+++ src/AppFive/app/src/main/java/ca/ualberta/appfive/OwnerInfo.java
@@ -1,12 +1,33 @@
 package ca.ualberta.appfive;
 
 /**
- * Created by waiyi on 3/7/16.
+ * A model for holding Owner info of books
  */
-public class OwnerInfo {
+public class OwnerInfo extends BModel<BView>{
 
     private String name;
     private String email;
+
+    /**
+     * This constructor is called when creating the Owner Info for the current user of the app.
+     * @param userProfile The current user's profile data
+     */
+    public OwnerInfo(UserProfile userProfile) {
+        super();
+        this.name = userProfile.getUserName();
+        this.email = userProfile.getUserEmail();
+    }
+
+    /**
+     * This is called when creating a book's Owner Info
+     * @param name Owner's User Name
+     * @param email Owner's contact info
+     */
+    public OwnerInfo(String name, String email) {
+        super();
+        this.name = name;
+        this.email = email;
+    }
 
     public String getName() {
         return name;
