ead72ccb1fb2c9b5bded8cc1729a9c29bf3a3557
omokdad <omokdad@yahoo.com>
2016-03-11 19:27:24 -0700

    temp commit to switch computers

--- src/AppFive/app/src/main/java/ca/ualberta/appfive/AppFive.java
+++ src/AppFive/app/src/main/java/ca/ualberta/appfive/AppFive.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 public class AppFive extends BModel<BView>{
 
     private UserProfile userProfile;
-    static private  ArrayList<OwnerInfo> owners = new ArrayList<OwnerInfo>();
-    static private  ArrayList<Book> books = new ArrayList<Book>();
+    static private ArrayList<Book> books = new ArrayList<Book>();
+    static private ArrayList<Book> mybooks = new ArrayList<Book>();
 
     AppFive() {
         super();
@@ -20,19 +20,53 @@ public class AppFive extends BModel<BView>{
         this.userProfile = UserProfile.getInstance();
     }
 
-    public static ArrayList<OwnerInfo> getOwners() {
-        return owners;
-    }
-
-    public OwnerInfo getOwner(int id) {
-        return owners.get(id);
-    }
-
     public static ArrayList<Book> getBooks(){
        return books;
     }
 
-    public Book getBook(int id){
-        return books.get(id);
+    public static Book getBook(int index){
+        return books.get(index);
     }
+
+    private static void addBook(Book book) {
+        books.add(book);
+        book.notifyViews();
+        //TODO update database
+    }
+
+    private static void deleteBook(int index) {
+        books.remove(index);
+        //TODO update database
+    }
+
+    public static void editBook(int index, Book newBook) {
+        // to edit a book construct a new book with the new attributes
+        // and call this function with the index of the old instance,
+        // and the new book to replace it.
+
+        Book oldBook = getBook(index);
+        oldBook.setDescription(newBook.getDescription());
+        oldBook.setGenre(newBook.getGenre());
+        oldBook.setTitle(newBook.getTitle());
+
+        //TODO update database
+    }
+
+
+
+
+    public static ArrayList<Book> getMybooks(){
+        return mybooks;
+    }
+
+    public static Book getMybook(int index){
+        return mybooks.get(index);
+    }
+
+    public static void addMybook(Book book) {}
+
+    public static void deleteMybook(int index) {}
+
+
+
 }
