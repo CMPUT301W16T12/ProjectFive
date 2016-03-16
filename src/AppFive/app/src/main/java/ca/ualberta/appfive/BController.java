d453d024c01c1738c855096d9f3c4c81a3f538b5
waiyi <waiyi@ualberta.ca>
2016-03-16 07:11:31 -0600

    Just trying out BController

--- src/AppFive/app/src/main/java/ca/ualberta/appfive/BController.java
+++ src/AppFive/app/src/main/java/ca/ualberta/appfive/BController.java
@@ -1,9 +1,24 @@
 package ca.ualberta.appfive;
 
+import android.graphics.Bitmap;
+
 /**
  * Controller class is implemented in this.
  *
  */
 public interface BController {
+    //Should we get this to exit the program??
+    public boolean isExit();
 
+    // do we get book and user like this?
+    public Book[] getBook();
+
+    // do we get user specifically of name and email
+    // or just this getUser??
+    public UserProfile[] getUser();
+
+    // this is what we learn from Lab yesterday
+    // needs to do more later
+    public Bitmap getMapBitmap();
 }
+
