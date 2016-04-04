235a19ad7c038c7f98f9ae5d1231685fdae0d20c
Justin Daza <justinjo@ualberta.ca>
2016-04-03 18:15:33 -0600

    Radiobuttons implemented -UNSTABLE

--- src/AppFive/app/src/main/java/ca/ualberta/appfive/MyBooksActivity.java
+++ src/AppFive/app/src/main/java/ca/ualberta/appfive/MyBooksActivity.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.PopupMenu;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,6 +25,7 @@ import java.util.concurrent.ExecutionException;
 public class MyBooksActivity extends AppCompatActivity implements BView<BModel>{
 
     BookListAdapter bla;
+    private RadioGroup radioButtonGroup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,18 +40,23 @@ public class MyBooksActivity extends AppCompatActivity implements BView<BModel>{
         // ac.addBook(new Book("test", "this is a test", "testing","thumbnail"));
 
 
-
         //TODO: Send to AppController
         //TODO: correct or not if I added this ES getbooktask, but not deleting adapter?
         // get the books by username from Elasticsearch
 
-        bla = new BookListAdapter(this, ac.getMyBookArray());
 
+        final ArrayList<Book> displayBooks;
+        final ArrayList<Book> tempBookArray;
+        displayBooks = ac.getMyBookArray();
+        tempBookArray = ac.getBookArray();
 
         Button addBookButton = (Button) findViewById(R.id.addBookButton);
-        ListView booksListView = (ListView) findViewById(R.id.LVMyBooks);
+        radioButtonGroup = (RadioGroup) findViewById(R.id.radioDisplayGroup);
+        bla = new BookListAdapter(this, displayBooks);
 
+        final ListView booksListView = (ListView) findViewById(R.id.LVMyBooks);
         booksListView.setAdapter(bla);
+
 
         // Behaviour for clicking on a book in the list
         //Regular Click views book
@@ -58,90 +65,85 @@ public class MyBooksActivity extends AppCompatActivity implements BView<BModel>{
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(MyBooksActivity.this, BookDisplayActivity.class);
-                intent.putExtra("INDEX",position);
+                intent.putExtra("INDEX", position);
                 intent.putExtra("MODE", BookDisplayActivity.DISPLAY_EDIT_MODE);
                 startActivity(intent);
             }
         });
         //Long click provides menu to edit or delete
         booksListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                PopupMenu popup = new PopupMenu(getApplicationContext(), view);
-                Menu editMenu = popup.getMenu();
-                MenuInflater inflater = popup.getMenuInflater();
-                inflater.inflate(R.menu.editmenu, popup.getMenu());
+             @Override
+             public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                 PopupMenu popup = new PopupMenu(getApplicationContext(), view);
+                 Menu editMenu = popup.getMenu();
+                 MenuInflater inflater = popup.getMenuInflater();
+                 inflater.inflate(R.menu.editmenu, popup.getMenu());
 
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
-                        Boolean result = null;
-                        ESController.DeleteBookTask deleteBookTask = new ESController.DeleteBookTask();
-                        deleteBookTask.execute(ac.getMyBook(position));
-                        try {
-                            result = deleteBookTask.get();
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        } catch (ExecutionException e) {
-                            e.printStackTrace();
-                        }
+                 popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                     @Override
+                     public boolean onMenuItemClick(MenuItem item) {
+                         Boolean result = null;
+                         ESController.DeleteBookTask deleteBookTask = new ESController.DeleteBookTask();
+                         deleteBookTask.execute(ac.getMyBook(position));
+                         try {
+                             result = deleteBookTask.get();
+                         } catch (InterruptedException e) {
+                             e.printStackTrace();
+                         } catch (ExecutionException e) {
+                             e.printStackTrace();
+                         }
 
-                        if(result == null){
-                            // Could not connect to database
-                            // TODO: Offline behaviour, store task and do when online again
+                         if (result == null) {
+                             // Could not connect to database
+                             // TODO: Offline behaviour, store task and do when online again
 
-                        } else if(result){
-                            // Successfully deleted book in database
-                            Log.d("MyBooksActivity", "onMenuItemClick: Deleted book successfully in database");
-                        } else {
-                            // This is reached when the database is not synced with the device
-                            Log.d("MyBooksActivity", "onMenuItemClick: connected but could not delete book in database");
-                        }
-                        // Delete book locally
-                        ac.deleteBook(position);
-                        return true;
-                    }
-                });
-                popup.show();
-                return true;
-            }
-        }
+                         } else if (result) {
+                             // Successfully deleted book in database
+                             Log.d("MyBooksActivity", "onMenuItemClick: Deleted book successfully in database");
+                         } else {
+                             // This is reached when the database is not synced with the device
+                             Log.d("MyBooksActivity", "onMenuItemClick: connected but could not delete book in database");
+                         }
+                         // Delete book locally
+                         ac.deleteBook(position);
+                         return true;
+                     }
+                 });
+                 popup.show();
+                 return true;
+             }
+         }
         );
         // Behaviour for clicking on add book button
-        addBookButton.setOnClickListener( new View.OnClickListener() {
+        addBookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MyBooksActivity.this, EditBookActivity.class);
                 startActivity(intent);
             }
         });
-    }
 
+        //Behavior for clicking something in radio button group
+        radioButtonGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton rb = (RadioButton) group.findViewById(checkedId);
+                switch (rb.getId()) {
+                    case R.id.radioButtonAll:
+                        
+                        break;
+                    case R.id.radioButtonAvailable:
+                        for(Book book:displayBooks) {
 
-    public void onRadioButtonClicked(View view) {
-        boolean checked = ((RadioButton)view).isChecked();
-        switch(view.getId()) {
-            case R.id.radioButtonAll:
-                if(checked){
-
+                        }
+                        break;
+                    case R.id.radioButtonBidded:
+                        break;
+                    case R.id.radioButtonBorrowed:
+                        break;
                 }
-                break;
-            case R.id.radioButtonAvailable:
-                if(checked){
-
-                }
-                break;
-            case R.id.radioButtonBidded:
-                if(checked){
-
-                }
-                break;
-            case R.id.radioButtonorrowed:
-                if(checked){
-
-                }
-                break;
-        }
+            }
+        });
 
     }
 
